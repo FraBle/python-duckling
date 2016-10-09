5c4760c4f8e0bf76e9018722e0de16f7f97a875d
Frank Blechschmidt <frank.blechschmidt@sap.com>
2016-10-09 01:43:49 -0700

    cleanup (requirements.txt, java code) and updating setup.cfg

--- src/PyDuckling.java
+++
@@ -1,9 +0,0 @@
-import duckling.core;
-
-public class PyDuckling {
-
-    public static void main(String[] args) {
-        core.load();
-        System.out.println("(binomial 5 3): " + core.parse("tomorrow"));
-    }
-}
