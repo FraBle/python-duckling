b97434935df3c4fd2238a6366e042ead31983838
Frank Blechschmidt <frank.blechschmidt@sap.com>
2016-10-17 13:45:45 -0700

    declared v1.1.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='duckling',
-    version='1.1.2',
+    version='1.1.3',
     description='A wrapper for wit.ai\'s Duckling',
     url='https://github.com/FraBle/python-duckling',
     author='Frank Blechschmidt',
