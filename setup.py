27dc590d0436a4994bdf64142b0839e12ea0bdd9
Frank Blechschmidt <frable90@gmail.com>
2018-04-15 02:36:25 +0200

    Declare v1.8.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='duckling',
-    version='1.7.3',
+    version='1.8.0',
     description='A wrapper for wit.ai\'s Duckling',
     url='https://github.com/FraBle/python-duckling',
     author='Frank Blechschmidt',
